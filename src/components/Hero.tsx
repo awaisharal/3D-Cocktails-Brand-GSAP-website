bc69c077ea732b0557716bca8c98bf0424564bca
awaisharal <awais.khalid800@gmail.com>
2025-09-21 15:33:46 +0500

    updates

---
+++ src/components/Hero.tsx
@@ -0,0 +1,127 @@
+"use client";
+import { useRef, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import SplitText from "gsap/SplitText";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+const Hero = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  gsap.registerPlugin(SplitText, ScrollTrigger);
+
+  useGSAP(() => {
+    const heroSplit = new SplitText(".title", {
+      type: "chars, words",
+    });
+
+    const paragraphSplit = new SplitText(".subtitle", {
+      type: "lines",
+    });
+
+    heroSplit.chars.forEach((char) =>
+      char.classList.add(`text-gradient`)
+    );
+
+    gsap.from(heroSplit.chars, {
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.06,
+    });
+
+    gsap.from(paragraphSplit.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.06,
+      delay: 1,
+    });
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0);
+
+    if (!videoRef.current) return;
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: videoRef.current,
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true
+      }
+    })
+
+    if(videoRef.current.readyState >= 2) {
+        tl.to(videoRef.current, {
+            currentTime: videoRef.current.duration,
+        });
+    }
+
+  }, []);
+
+  return (
+    <>
+      <section id="hero" className="noisy">
+        <h1 className="title">MOJITO</h1>
+        <img
+          src="images/hero-left-leaf.png"
+          alt="Hero Left Leaf"
+          className="left-leaf"
+        />
+        <img
+          src="images/hero-right-leaf.png"
+          alt="Hero Right Leaf"
+          className="right-leaf"
+        />
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cool. Crisp. Classic.</p>
+              <p className="subtitle">
+                Sip the Spirit <br /> of Summer
+              </p>
+            </div>
+
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every cocktail on our menu is a blend of premium ingredients,
+                creative flair, and timeless recipes - designed to delight your
+                senses.
+              </p>
+              <a href="#cocktails">View our cocktails</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Hero;
